0bf92afe9d4a4d2089ae3ff14057ea84f5cfbe1f
vuthanhptnkmai <vuthanhptnkmai@gmail.com>
2023-10-17 18:20:58 +0200

    class for handling rays

--- simulator/ray.h
+++ simulator/ray.h
@@ -2,9 +2,10 @@
 
 #include "../utils/vec3.h"
 
+// Declaration
 template<typename T, typename U>
 class Ray {
-public: // here it should be protected instead .. try out different methods for more information, write functions for getter and setter
+private:
     vec3<T> position;
     vec3<T> direction;
     U wavelength;
@@ -12,8 +13,60 @@ public: // here it should be protected instead .. try out different methods for 
 
 public:
     Ray(const vec3<T>& position, const vec3<T>& direction, U wavelength, U intensity);
+
+    const vec3<T>& getPosition() const;
+    const vec3<T>& getDirection() const;
+    U getWavelength() const;
+    U getIntensity() const;
+
+    void setPosition(const vec3<T>& pos);
+    void setDirection(const vec3<T>& dir);
+    void setWavelength(U wl);
+    void setIntensity(U inten);
 };
+
+// Definitions
 
 template<typename T, typename U>
 Ray<T, U>::Ray(const vec3<T>& position, const vec3<T>& direction, U wavelength, U intensity)
     : position(position), direction(direction), wavelength(wavelength), intensity(intensity) {}
+
+template<typename T, typename U>
+const vec3<T>& Ray<T, U>::getPosition() const {
+    return position;
+}
+
+template<typename T, typename U>
+const vec3<T>& Ray<T, U>::getDirection() const {
+    return direction;
+}
+
+template<typename T, typename U>
+U Ray<T, U>::getWavelength() const {
+    return wavelength;
+}
+
+template<typename T, typename U>
+U Ray<T, U>::getIntensity() const {
+    return intensity;
+}
+
+template<typename T, typename U>
+void Ray<T, U>::setPosition(const vec3<T>& pos) {
+    position = pos;
+}
+
+template<typename T, typename U>
+void Ray<T, U>::setDirection(const vec3<T>& dir) {
+    direction = dir;
+}
+
+template<typename T, typename U>
+void Ray<T, U>::setWavelength(U wl) {
+    wavelength = wl;
+}
+
+template<typename T, typename U>
+void Ray<T, U>::setIntensity(U inten) {
+    intensity = inten;
+}
